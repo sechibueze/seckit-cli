ca8e30977bb15f708100ef339bb4d9b812ab00a5
Samuel Chibueze <sechibueze@gmail.com>
2025-05-14 21:55:29 +0100

    Add file compression with file compression stats

--- cli.ts
+++ cli.ts
@@ -2,12 +2,37 @@
 import fs from "node:fs";
 import { program } from "commander";
 import inquirer from "inquirer";
+import { compressFile } from "./lib/compress";
 program
   .name("seckit-cli")
   .version("1.0.0")
   .description(
     "🔐 A CLI tool for compressing, encrypting, and uploading files"
   );
+
+async function runInInteractiveMode() {
+  const answer = await inquirer.prompt([
+    {
+      type: "list",
+      name: "action",
+      message: "What do you want to do?",
+      choices: ["Compress Files", "Encrypt Files", "Upload Files"],
+    },
+  ]);
+
+  if (answer.action === "Compress Files") {
+    const { fileLocation } = await inquirer.prompt([
+      {
+        type: "input",
+        message: "Enter file path",
+        name: "fileLocation",
+      },
+    ]);
+
+    console.log("compressing file", fileLocation);
+    compressFile(fileLocation);
+  }
+}
 
 program
   .command("compress")
@@ -20,3 +45,9 @@ program
 
     // readStream.pipe(writeStream);
   });
+
+if (process.argv.length <= 2) {
+  runInInteractiveMode();
+} else {
+  program.parse(process.argv);
+}
